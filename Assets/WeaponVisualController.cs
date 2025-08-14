a13fa3387d2ce007edfd5b28a5427e88276e0b38
Chris <little-god@web.de>
2025-08-14 08:45:53 +0200

    added guns and transforms. Tutorial 25 Minute 10

---
+++ Assets/WeaponVisualController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class WeaponVisualController : MonoBehaviour
+{
+    [SerializeField] private Transform[] gunTransforms;
+    [SerializeField] private Transform pistol;
+    [SerializeField] private Transform revolver;
+    [SerializeField] private Transform autoRifle;
+    [SerializeField] private Transform shotgun;
+    [SerializeField] private Transform SniperRifle;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
